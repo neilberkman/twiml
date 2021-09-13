357f866460d55260a8c2a1f74025ada25691876c
Mario Uher <uher.mario@gmail.com>
2021-09-13 13:49:34 +0200

    Prepare release

--- mix.exs
+++ mix.exs
@@ -2,6 +2,8 @@ defmodule Twiml.MixProject do
   use Mix.Project
 
   @version "0.1.0"
+  @url "https://github.com/YodelTalk/twiml"
+  @maintainers ["Christoph Leitner", "Mario Uher"]
 
   def project do
     [
@@ -10,7 +12,13 @@ defmodule Twiml.MixProject do
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      docs: [extras: ["README.md"], main: "readme", source_ref: "v#{@version}"]
+      description: description(),
+      package: package(),
+
+      # Docs
+      name: "TwiML",
+      source_url: @url,
+      docs: [main: "TwiML", source_ref: "v#{@version}"]
     ]
   end
 
@@ -19,6 +27,20 @@ defmodule Twiml.MixProject do
     [
       {:xml_builder, "~> 2.2"},
       {:ex_doc, "~> 0.24", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    """
+    Generate complex TwiML responses for Twilio in an elegant Elixir way.
+    """
+  end
+
+  defp package() do
+    [
+      maintainers: @maintainers,
+      licenses: ["MIT"],
+      links: %{"GitHub" => @url}
     ]
   end
 end
